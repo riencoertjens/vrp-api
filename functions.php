63c29911844d0bf8106592b0a70eebfa9aadd18f
Rien Coertjens <rien.coertjens@gmail.com>
2019-06-05 10:17:36 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -37,7 +37,7 @@ function refreshSite($post_id){
 } add_action( 'save_post', 'refreshSite', 11, 1 );
 
 //Auto add and update Title field:
-function my_post_title_updater( $post_id ) {
+function update_ruimte_post_title( $post_id ) {
 
 	$my_post = array();
 	$my_post['ID'] = $post_id;
@@ -48,11 +48,24 @@ function my_post_title_updater( $post_id ) {
 	// Update the post into the database
 	wp_update_post( $my_post );
 
-}
+} add_action('acf/save_post', 'update_ruimte_post_title', 10, 1);
 
+//Auto update Prijs category:
+function update_prijs_post_category( $post_id ) {
 
-// run after ACF saves the $_POST['fields'] data
-add_action('acf/save_post', 'my_post_title_updater', 10, 1);
+	if ( get_post_type() == 'prijs' ) {
+		$parent_cat_id = get_category_by_slug('prijzen')->cat_ID;
+		$prijs_cat_id = get_category_by_slug(get_field('prijs'))->cat_ID;
+		
+		$categories = array();
+		foreach (get_the_category() as $category) {
+			if ($category->parent != $parent_cat_id) array_push($categories, $category->cat_ID);
+		}
+		array_push($categories, $prijs_cat_id);
+		wp_set_post_categories($post_id, $categories);
+	}
+
+} add_action('acf/save_post', 'update_prijs_post_category', 15, 1);
 
 
 /*
