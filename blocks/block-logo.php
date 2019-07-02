01a6e16132ba9453e7c0a563f6d29c7d5a9fa0d3
Rien Coertjens <rien.coertjens@gmail.com>
2019-07-02 11:49:58 +0200

    edit logo link

--- blocks/block-logo.php
+++ blocks/block-logo.php
@@ -1,4 +1,4 @@
-<a href="<?php block_field( 'link' ); ?>" class="logo_block">
+<a href="<?php block_field( 'link' ); ?>" class="logo_block" target="_blank" rel="noreferrer noopener">
   <?php echo wp_get_attachment_image( block_value( 'image' ), 'original' ); ?>
   <span><?php block_field( 'name' ); ?></span>
 </a>
