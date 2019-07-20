6d31eda579ba34a3237edaac9189f79fca4f92f3
Rien Coertjens <rien.coertjens@gmail.com>
2019-07-20 10:53:53 +0200

    inline images / links

--- plugins/Wordlify/prepareData/makeImagesRelative.php
+++ plugins/Wordlify/prepareData/makeImagesRelative.php
@@ -13,7 +13,7 @@ function makeInlineImagesRelative($post_content) {
     $url = preg_quote(get_site_url(), "/");
 
     return preg_replace(
-        "/$url\/wp-content\//", '../../static/wordsby/', $post_content
+        "/$url\/wp-content\//", '/static/wordsby/', $post_content
     );
 }
 
