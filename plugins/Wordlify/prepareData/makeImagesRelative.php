37a6b42ca462da48609ccb784d93a4eeaaa688cf
Rien Coertjens <rien.coertjens@gmail.com>
2019-07-20 13:03:30 +0200

    rm vrp.be from content

--- plugins/Wordlify/prepareData/makeImagesRelative.php
+++ plugins/Wordlify/prepareData/makeImagesRelative.php
@@ -1,10 +1,11 @@
 <?php 
 
 function makeImagesRelative($json) {
+
     $url = preg_quote(get_site_url(), "/");
 
     return preg_replace(
-        "/$url\/wp-content\//", '../../static/wordsby/', $json
+        "/$url\/wp-content\//", '../../static/wordsby/', str_replace('vrp.be', 'webhart.one', $json)
     );
 }
 
