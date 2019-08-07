114d26f1dbfc4c9a5f966c7f44771612119459d3
Rien Coertjens <rien.coertjens@gmail.com>
2019-08-07 14:08:21 +0200

    vrp urls to https

--- plugins/Wordlify/prepareData/makeImagesRelative.php
+++ plugins/Wordlify/prepareData/makeImagesRelative.php
@@ -2,9 +2,9 @@
 
 function makeImagesRelative($json) {
     $url = preg_quote(get_site_url(), "/");
-
+    $content = str_replace(array('http://www.vrp.be', 'http://.vrp.be', 'https://vrp.be'), 'https://www.vrp.be', $json);
     return preg_replace(
-        "/$url\/wp-content\//", '../../static/wordsby/', $json
+        "/$url\/wp-content\//", '../../static/wordsby/', $content
     );
 }
 
