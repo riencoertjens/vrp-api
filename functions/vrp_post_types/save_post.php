ebd660763213185cb775203e8aff1f64c4853d70
Rien Coertjens <rien.coertjens@gmail.com>
2019-08-08 17:08:51 +0200

    ruimte title subject

--- functions/vrp_post_types/save_post.php
+++ functions/vrp_post_types/save_post.php
@@ -1,26 +1,34 @@
 <?php
 //Auto add and update Title field for post_type ruimte:
-function update_ruimte_post_title( $post_id ) {
+function update_ruimte_post_title($post_id)
+{
 
 	$my_post = array();
 	$my_post['ID'] = $post_id;
 
-	if ( get_post_type() == 'ruimte' ) {
-		$my_post['post_title'] = 'Ruimte ' . get_field('nummer');
-		$my_post['post_name'] = 'ruimte-' . get_field('nummer');
-	} 
-	// Update the post into the database
-	wp_update_post( $my_post );
+	if (get_post_type() == 'ruimte') {
+		$title = 'Ruimte ' . get_field('nummer');
+		$subject = get_field('subject');
+		if ($subject && strlen($subject) > 1) {
+			$title .= " " . get_field('subject');
+		}
+		$my_post['post_title'] = $title;
 
-} add_action('acf/save_post', 'update_ruimte_post_title', 10, 1);
+		$my_post['post_name'] = 'ruimte-' . get_field('nummer');
+	}
+	// Update the post into the database
+	wp_update_post($my_post);
+}
+add_action('acf/save_post', 'update_ruimte_post_title', 10, 1);
 
 //Auto update Prijs category for post_type prijs:
-function update_prijs_post_category( $post_id ) {
+function update_prijs_post_category($post_id)
+{
 
-	if ( get_post_type() == 'prijs' ) {
+	if (get_post_type() == 'prijs') {
 		$parent_cat_id = get_category_by_slug('prijzen')->cat_ID;
 		$prijs_cat_id = get_category_by_slug(get_field('prijs'))->cat_ID;
-		
+
 		$categories = array();
 		foreach (get_the_category() as $category) {
 			if ($category->parent != $parent_cat_id) array_push($categories, $category->cat_ID);
@@ -28,5 +36,5 @@ function update_prijs_post_category( $post_id ) {
 		array_push($categories, $prijs_cat_id);
 		wp_set_post_categories($post_id, $categories);
 	}
-
-} add_action('acf/save_post', 'update_prijs_post_category', 15, 1);
+}
+add_action('acf/save_post', 'update_prijs_post_category', 15, 1);
